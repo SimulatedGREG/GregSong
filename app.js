8aafd0f2f8e7e89f04356ce1abd999ff2a08d7ff
SimulatedGREG <simulatedgreg@gmail.com>
2016-01-08 14:43:29 -0600

    nearly all logic, needs front-end

--- app.js
+++ app.js
@@ -1,43 +1,77 @@
 var fs = require('fs');
 
-var GregSong = new Object.create({
-	currentStatus: {
-		title: '',
-		artist: '',
-		action: 'stopped'
-	}
+/**
+ * Create base object and define global variables.
+ */
+var GregSong = Object.create({
+  path: './test.txt',
+  status: {
+    title: '',
+    artist: '',
+    action: ''
+  }
 });
 
+/**
+ * Initialize
+ */
 GregSong._init = function() {
-	if(this.checks()){
-		this.watcher();
-	}
+  // TODO: Give meaningful feedback to user on missing file.
+  if(this.check())
+    this.watcher();
+  else
+    console.error('File not found at ' + this.path);
 };
 
-GregSong.checks = function() {
-
+/**
+ * Check if file is accessable.
+ * @return {boolean} Does file exist.
+ */
+GregSong.check = function() {
+  try {
+    fs.accessSync(this.path, fs.F_OK);
+    return true;
+  } catch (err) { return false; }
 };
 
+/**
+ * Watch for file changes and call to analyze if changed.
+ */
 GregSong.watcher = function() {
-	fs.watchFile(process.env.USERPROFILE + '/Desktop/text.txt',
-		function(curr, prev) {
-			fs.readFile(process.env.USERPROFILE + '/Desktop/text.txt',
-				'utf8',
-				function(err, data) {
-					if(err) throw err;
-					console.log(data);
-				});
-		});
+  fs.watchFile(GregSong.path, function(curr, prev) {
+    GregSong.analyze(fs.readFileSync(GregSong.path, 'ascii').replace(/(\r\n|\n|\r|(o;\?))/gm, ''));
+  });
 };
 
-GregSong.onPlay = function() {
+/**
+ * Updates current status and notifies if media playing.
+ * @param  {string} data String read from text file.
+ */
+GregSong.analyze = function(data) {
+  if(data == 'stopped') {
+    this.status = {
+      action: data,
+      title: '',
+      artist: ''
+    };
+  } else {
+    this.status = {
+      action: data.split(':')[0],
+      title: data.split(':')[1].split(' - ')[1],
+      artist: data.split(': ')[1].split(' - ')[0]
+    };
+  }
 
+  if(this.status.action === 'playing'){
+		GregSong.notify();}
 };
 
-GregSong.onPause = function() {
-
+/**
+ * Pushes status to DOM and performs animation.
+ * TODO: Connect with DOM.
+ */
+GregSong.notify = function() {
+  console.log('hello DOM');
 };
 
-GregSong.onStop = function() {
-
-};
+GregSong._init();
